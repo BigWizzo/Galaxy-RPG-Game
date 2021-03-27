9f6fecd5f3b75d737556c6096c8e0f19593d5696
bigwizzo <wnnwillard@gmail.com>
2021-03-27 22:40:38 +0200

    Add sprites to the GameScene

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -2,7 +2,9 @@ import 'phaser';
 import diamond from '../assets/game/diamond.png';
 import platform from '../assets/game/platform.png';
 import sky from '../assets/game/sky.png';
+import background from '../assets/game/background.jpg';
 import woof from '../assets/game/woof.png';
+import dragon from '../assets/game/dragonorrange.png';
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -13,13 +15,29 @@ export default class GameScene extends Phaser.Scene {
     // load images
     this.load.image('diamond', diamond);
     this.load.image('platform', platform);
-    this.load.image('sky', sky);
+    this.load.image('background', background);
+    this.load.image('dragon', dragon);
     this.load.image('woof', woof);
   }
 
   create () {
     // this.physics.startSystem(Phaser.Physics.ARCADE)
-    let bg = this.add.sprite(0, 0, 'sky');
+    let bg = this.add.sprite(0, 0, 'background');
     bg.setOrigin(0,0);
+
+    this.woof = this.add.sprite(64, 64, 'woof')
+    this.dragon1 = this.add.sprite(100, 100, 'dragon')
+    this.dragon2 = this.add.sprite(32, 32, 'dragon')
+    this.dragon3 = this.add.sprite(150, 150, 'dragon')
+  }
+
+  update() {
+    this.moveDragon(this.dragon1, 1);
+    this.moveDragon(this.dragon2, 2);
+    this.moveDragon(this.dragon3, 3);
+  }
+
+  moveDragon(dragon, speed) {
+    dragon.y += speed;
   }
 };
