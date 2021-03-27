62d0d4bddb4d4885a2b3ada4634d0e2fe11ae9a2
bigwizzo <wnnwillard@gmail.com>
2021-03-28 01:01:47 +0200

    Add enemy power balls

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -6,6 +6,7 @@ import background from '../assets/game/background.jpg';
 import woof from '../assets/game/woof.png';
 import dragon from '../assets/game/dragonorrange.png';
 import ship2 from '../assets/game/ship2.png';
+import power from '../assets/game/power.png';
 import explosion from '../assets/game/explosion.png';
 import config from '../Config/config';
 
@@ -24,7 +25,15 @@ export default class GameScene extends Phaser.Scene {
       frameWidth: 16,
       frameHeight: 16
     });
+    this.load.spritesheet('power', power, {
+      frameWidth: 16,
+      frameHeight: 16
+    });
     this.load.spritesheet('woof', woof, {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet('explosion', explosion, {
       frameWidth: 32,
       frameHeight: 32
     });
@@ -41,9 +50,58 @@ export default class GameScene extends Phaser.Scene {
     this.dragon2 = this.add.sprite(32, 32, 'dragon')
     this.dragon3 = this.add.sprite(150, 150, 'dragon')
 
-    // const gameW = this.sys.config.height;
-// 
-    // console.log(gameW)
+    this.anims.create({
+      key: "explode",
+      frames: this.anims.generateFrameNumbers("explosion"),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
+    });
+
+    this.anims.create({
+      key: "red",
+      frames: this.anims.generateFrameNumbers("power", {
+        start: 0,
+        end: 1
+      }),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "gray",
+      frames: this.anims.generateFrameNumbers("power", {
+        start: 2,
+        end: 3
+      }),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.physics.world.setBoundsCollision();
+
+    this.powerUps = this.physics.add.group();
+
+    var maxObjects = 4;
+    for (var i = 0; i <= maxObjects; i++) {
+      var powerUp = this.physics.add.sprite(16, 16, "power-up");
+      this.powerUps.add(powerUp);
+       powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+
+      // set random animation
+      if (Math.random() > 0.5) {
+        powerUp.play("red");
+      } else {
+        powerUp.play("gray");
+      }
+
+      // setVelocity
+      powerUp.setVelocity(50, 50);
+      // 3.2
+      powerUp.setCollideWorldBounds(true);
+      // 3.3
+     powerUp.setBounce(1);
+
+    }
   }
 
   update() {
