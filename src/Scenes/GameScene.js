f75d937739bee583f46bbe5c0aeb708e1298650f
bigwizzo <wnnwillard@gmail.com>
2021-03-28 02:23:42 +0200

    Add ship woof

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -4,8 +4,8 @@ import platform from '../assets/game/platform.png';
 import sky from '../assets/game/sky.png';
 import background from '../assets/game/background.jpg';
 import woof from '../assets/game/woof.png';
-import dragon from '../assets/game/dragonorrange.png';
-import ship2 from '../assets/game/ship2.png';
+// import dragon from '../assets/game/dragonorrange.png';
+// import ship2 from '../assets/game/ship2.png';
 import power from '../assets/game/power.png';
 import explosion from '../assets/game/explosion.png';
 import config from '../Config/config';
@@ -20,18 +20,18 @@ export default class GameScene extends Phaser.Scene {
     this.load.image('diamond', diamond);
     this.load.image('platform', platform);
     this.load.image('background', background);
-    this.load.image('dragon', dragon);
-    this.load.spritesheet('ship2', ship2, {
-      frameWidth: 16,
-      frameHeight: 16
-    });
+    // this.load.image('dragon', dragon);
+    // this.load.spritesheet('ship2', ship2, {
+      // frameWidth: 16,
+      // frameHeight: 16
+    // });
     this.load.spritesheet('power', power, {
       frameWidth: 16,
       frameHeight: 16
     });
     this.load.spritesheet('woof', woof, {
-      frameWidth: 32,
-      frameHeight: 32
+      frameWidth: 64,
+      frameHeight: 64
     });
     this.load.spritesheet('explosion', explosion, {
       frameWidth: 32,
@@ -45,10 +45,10 @@ export default class GameScene extends Phaser.Scene {
     bg.setOrigin(0,0);
 
     this.woof = this.add.sprite(64, 64, 'woof')
-    this.ship2 = this.add.sprite(140, 180, 'ship2')
-    this.dragon1 = this.add.sprite(100, 100, 'dragon')
-    this.dragon2 = this.add.sprite(32, 32, 'dragon')
-    this.dragon3 = this.add.sprite(150, 150, 'dragon')
+    // this.ship2 = this.add.sprite(140, 180, 'ship2')
+    // this.dragon1 = this.add.sprite(100, 100, 'dragon')
+    // this.dragon2 = this.add.sprite(32, 32, 'dragon')
+    // this.dragon3 = this.add.sprite(150, 150, 'dragon')
 
     this.anims.create({
       key: "explode",
@@ -105,10 +105,10 @@ export default class GameScene extends Phaser.Scene {
   }
 
   update() {
-    this.moveDragon(this.dragon1, 1);
-    this.moveDragon(this.dragon2, 2);
-    this.moveDragon(this.dragon3, 3);
-    this.moveDragon(this.ship2, 3);
+    // this.moveDragon(this.dragon1, 1);
+    // this.moveDragon(this.dragon2, 2);
+    // this.moveDragon(this.dragon3, 3);
+    // this.moveDragon(this.ship2, 3);
   }
 
   moveDragon(dragon, speed) {
