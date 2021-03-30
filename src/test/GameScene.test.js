77e62caf96605c181a59a439b64492ca5fc4a3df
bigwizzo <wnnwillard@gmail.com>
2021-03-30 23:20:55 +0200

    Test GameScene functions

--- src/test/GameScene.test.js
+++ src/test/GameScene.test.js
@@ -5,8 +5,28 @@ import GameScene from '../Scenes/GameScene';
 describe('GameScene class', () => {
   const myGameScene = new GameScene();
 
-  test('initial display of tabs', () => {
+  test('pick points when woof collides with powerUps', () => {
     expect(typeof myGameScene.pickPowerUp).toBe('function');
+  });
+
+  test('pick points when woof collides with powerUps', () => {
+    expect(typeof myGameScene.pickPowerUp).not.toBe('object');
+  });
+
+  test('player woof dies when collides with danger', () => {
+    expect(typeof myGameScene.pickDanger).toBe('function');
+  });
+  
+  test('player woof dies when collides with danger', () => {
+    expect(typeof myGameScene.pickDanger).not.toBe('object');
+  });
+
+  test('sets velocity for player woof movement', () => {
+    expect(typeof myGameScene.moveWoofManager).toBe('function');
+  });
+    
+  test('sets velocity for player woof movement', () => {
+    expect(typeof myGameScene.moveWoofManager).not.toBe('object');
   });
   
 });
