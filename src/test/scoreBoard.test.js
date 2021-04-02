973cca916043c768e1b6e9a7dd869fb93265f720
bigwizzo <wnnwillard@gmail.com>
2021-04-02 18:30:05 +0200

    Add API tests

--- src/test/scoreBoard.test.js
+++ src/test/scoreBoard.test.js
@@ -1,28 +1,41 @@
-import 'phaser';
+import 'jest-canvas-mock';
 import { scoreBoard, setScore, sortScores } from '../Api/scoreBoard';
 
-require('jest-canvas-mock');
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve({ result: {} }),
+}));
 
-test('scoreBoard is a function', () => {
-  expect(typeof scoreBoard).toBe('function');
+beforeEach(() => {
+  fetch.mockClear();
 });
 
-test('scoreBoard is not an object', () => {
-  expect(typeof scoreBoard).not.toBe('object');
-});
+describe('Requestion the information from the API', () => {
+  it('The result should be an Object', async () => {
+    const result = await scoreBoard();
+    expect(typeof result).toBe('object');
+  });
 
-test('sortScores is a function', () => {
-  expect(typeof sortScores).toBe('function');
-});
+  test('scoreBoard is a function', () => {
+    expect(typeof scoreBoard).toBe('function');
+  });
 
-test('sortScores is not an object', () => {
-  expect(typeof sortScores).not.toBe('object');
-});
+  test('scoreBoard is not an object', () => {
+    expect(typeof scoreBoard).not.toBe('object');
+  });
 
-test('setScore is a function', () => {
-  expect(typeof setScore).toBe('function');
-});
+  test('sortScores is a function', () => {
+    expect(typeof sortScores).toBe('function');
+  });
 
-test('setScore is not an object', () => {
-  expect(typeof setScore).not.toBe('object');
+  test('sortScores is not an object', () => {
+    expect(typeof sortScores).not.toBe('object');
+  });
+
+  test('setScore is a function', () => {
+    expect(typeof setScore).toBe('function');
+  });
+
+  test('setScore is not an object', () => {
+    expect(typeof setScore).not.toBe('object');
+  });
 });
